a9f54c00cd764bdcac51ee959cc69ac6f450a55d
PlutusDevServer <contact@muesliswap.com>
2024-02-23 15:41:01 +0100

    restructure

---
+++ onchain/stake_state.py
@@ -0,0 +1,117 @@
+from opshin.std.math import *
+from onchain.util import *
+from onchain.stake_state_types import *
+from onchain.batching_types import *
+
+
+# HELPER FUNCTIONS #####################################################################################################
+def resolve_linear_input_state(datum: StakingState) -> StakingState:
+    """
+    Resolve the datum of the input that is referenced by the redeemer.
+    """
+    # TODO could compare datum with previous_state_input.datum, but maybe not necessary
+    return datum
+
+
+def resolve_linear_output_state(
+    next_state_output: TxOut, tx_info: TxInfo
+) -> StakingState:
+    """
+    Resolve the continuing datum of the output that is referenced by the redeemer.
+    """
+    next_state: StakingState = resolve_datum_unsafe(next_state_output, tx_info)
+    return next_state
+
+
+def compute_updated_cumulative_reward_per_token(
+    prev_cum_rpt: int,
+    emission_rate: int,
+    last_update_time: POSIXTime,
+    current_time: POSIXTime,
+) -> int:
+    """
+    Compute the updated cumulative reward per token.
+    """
+    time_diff = current_time - last_update_time
+    # TODO divide by 24*60*60*1000 and round to get the emission rate per day
+    return prev_cum_rpt + (emission_rate * time_diff)
+
+
+# VALIDATOR ############################################################################################################
+def validator(
+    state: StakingState,
+    redeemer: StakeStateRedeemer,
+    context: ScriptContext,
+) -> None:
+    purpose = get_spending_purpose(context)
+    tx_info = context.tx_info
+    outputs = tx_info.outputs
+
+    previous_state_input = resolve_linear_input(
+        tx_info, redeemer.state_input_index, purpose
+    )
+    previous_state = resolve_linear_input_state(state)
+
+    next_state_output = resolve_linear_output(
+        previous_state_input, tx_info, redeemer.state_output_index
+    )
+    next_stake_state = resolve_linear_output_state(next_state_output, tx_info)
+
+    # TODO in any case, check that cumulative reward per token is updated correctly
+
+    if isinstance(redeemer, ApplyOrders):
+        # TODO additionally check that amount of staked tokens is updated correctly (with new / unstaked positions)
+
+        order_input = resolve_linear_input(tx_info, redeemer.order_input_index, purpose)
+        order_datum: AddStakeOrder = resolve_datum_unsafe(order_input, tx_info)
+        provided_amount = amount_of_token_in_output(
+            previous_state.params.stake_token, order_input
+        )
+        desired_amount_staked = previous_state.amount_staked + provided_amount
+
+        stake_txout = outputs[redeemer.order_output_index]
+        assert (
+            stake_txout.address == previous_state_input.address
+        ), "Referenced order output index does not send funds to staking address."
+        stake_datum: StakingPosition = resolve_datum_unsafe(stake_txout, tx_info)
+        assert isinstance(
+            stake_datum, StakingPosition
+        ), "Invalid staking position datum."
+        assert stake_datum.pool_id == order_datum.pool_id, "Pool ID mismatch."
+        assert (
+            next_stake_state.cumulative_reward_per_token
+            == stake_datum.cumulative_pool_rpt_at_start
+        ), "Cumulative reward per token set incorrectly in staking position datum."
+        assert contained_ext(
+            tx_info.valid_range, stake_datum.staked_since
+        ), "Invalid staking time."
+        assert range_is_short(
+            tx_info.valid_range, 5 * 60 * 1000
+        ), "Validity range longer than threshold."
+
+        staked_amount = amount_of_token_in_output(
+            previous_state.params.stake_token, stake_txout
+        )
+        assert staked_amount == provided_amount, "Staked amount < provided amount."
+
+    elif isinstance(redeemer, UpdateParams):
+        assert False, "UpdateParams not implemented yet."
+
+    else:
+        assert False, "Invalid redeemer."
+
+    desired_next_state = StakingState(
+        previous_state.params,
+        previous_state.emission_rate,
+        redeemer.current_time,
+        desired_amount_staked,
+        compute_updated_cumulative_reward_per_token(
+            previous_state.cumulative_reward_per_token,
+            previous_state.emission_rate,
+            previous_state.last_update_time,
+            redeemer.current_time,
+        ),
+    )
+    assert (
+        desired_next_state == next_stake_state
+    ), "Staking state not updated correctly."
--- onchain/staking.py
+++ onchain/staking.py
@@ -1,45 +1,12 @@
 from opshin.std.math import *
 from onchain.util import *
 from onchain.staking_types import *
-from onchain.batching_types import *
-
-
-# HELPER FUNCTIONS #####################################################################################################
-def resolve_linear_input_state(datum: StakingState) -> StakingState:
-    """
-    Resolve the datum of the input that is referenced by the redeemer.
-    """
-    # TODO could compare datum with previous_state_input.datum, but maybe not necessary
-    return datum
-
-
-def resolve_linear_output_state(
-    next_state_output: TxOut, tx_info: TxInfo
-) -> StakingState:
-    """
-    Resolve the continuing datum of the output that is referenced by the redeemer.
-    """
-    next_state: StakingState = resolve_datum_unsafe(next_state_output, tx_info)
-    return next_state
-
-
-def compute_updated_cumulative_reward_per_token(
-    prev_cum_rpt: int,
-    emission_rate: int,
-    last_update_time: POSIXTime,
-    current_time: POSIXTime,
-) -> int:
-    """
-    Compute the updated cumulative reward per token.
-    """
-    time_diff = current_time - last_update_time
-    # TODO divide by 24*60*60*1000 and round to get the emission rate per day
-    return prev_cum_rpt + (emission_rate * time_diff)
+from onchain.stake_state_types import *
 
 
 # VALIDATOR ############################################################################################################
 def validator(
-    state: StakingState,
+    datum: StakingPosition,
     redeemer: StakingRedeemer,
     context: ScriptContext,
 ) -> None:
@@ -47,77 +14,38 @@ def validator(
     tx_info = context.tx_info
     outputs = tx_info.outputs
 
-    previous_state_input = resolve_linear_input(
+    stake_state_input = resolve_linear_input(
         tx_info, redeemer.state_input_index, purpose
     )
-    previous_state = resolve_linear_input_state(state)
+    stake_state: StakingState = resolve_datum_unsafe(stake_state_input, tx_info)
+    assert token_present_in_output(
+        stake_state.params.pool_auth_nft, stake_state_input
+    ), "Pool auth NFT not present in stake_state_input."
+    assert (
+        datum.pool_id == stake_state.params.pool_auth_nft.token_name
+    ), "Referenced wrong pool in stake_state_input."
 
-    next_state_output = resolve_linear_output(
-        previous_state_input, tx_info, redeemer.state_output_index
-    )  # TODO need to allow multiple outputs to staking contract here (at least for ApplyOrders)
-    next_stake_state = resolve_linear_output_state(next_state_output, tx_info)
+    staking_position_input = resolve_linear_input(
+        tx_info, redeemer.order_input_index, purpose
+    )
 
-    if isinstance(redeemer, ApplyOrders):
-        order_input = resolve_linear_input(tx_info, redeemer.order_input_index, purpose)
-        order_datum: AddStakeOrder = resolve_datum_unsafe(order_input, tx_info)
-        provided_amount = amount_of_token_in_output(
-            previous_state.params.stake_token, order_input
-        )
-        desired_amount_staked = previous_state.amount_staked + provided_amount
+    if isinstance(redeemer, Unstake):
+        assert user_signed_tx(datum.owner, tx_info), "Owner did not sign transaction."
 
-        stake_txout = outputs[redeemer.order_output_index]
+        amount_of_token_in_output(stake_state.params.stake_token, stake_state_input)
+        total_rewards = 0
+        for o in outputs:
+            total_rewards += amount_of_token_in_output(
+                stake_state.params.reward_token, o
+            )
+        expected_rewards = (
+            stake_state.cumulative_reward_per_token - datum.cumulative_pool_rpt_at_start
+        ) * amount_of_token_in_output(
+            stake_state.params.stake_token, staking_position_input
+        )  # TODO change cum_rpt to value from stake_state output
         assert (
-            stake_txout.address == previous_state_input.address
-        ), "Referenced order output index does not send funds to staking address."
-        stake_datum: StakingPosition = resolve_datum_unsafe(stake_txout, tx_info)
-        assert isinstance(
-            stake_datum, StakingPosition
-        ), "Invalid staking position datum."
-        assert stake_datum.pool_id == order_datum.pool_id, "Pool ID mismatch."
-        assert (
-            next_stake_state.cumulative_reward_per_token
-            == stake_datum.cumulative_pool_rpt_at_start
-        ), "Cumulative reward per token set incorrectly in staking position datum."
-        assert contained_ext(
-            tx_info.valid_range, stake_datum.staked_since
-        ), "Invalid staking time."
-        assert range_is_short(
-            tx_info.valid_range, 5 * 60 * 1000
-        ), "Validity range longer than threshold."
-
-        staked_amount = amount_of_token_in_output(
-            previous_state.params.stake_token, stake_txout
-        )
-        assert staked_amount == provided_amount, "Staked amount < provided amount."
-
-    elif isinstance(redeemer, Unstake):
-        # TODO make sure owner (written in staking position's datum (TODO define this)) has signed tx
-        # TODO check that paid out amount is no more than rewards earned since start of staking (TODO compute from cum_rpt at staking time stored in staking position's datum)
-        desired_amount_staked = (
-            previous_state.amount_staked
-        )  # TODO subtract amount unstaked
-        pass
-
-    elif isinstance(redeemer, UpdateParams):
-        desired_amount_staked = previous_state.amount_staked
-        # TODO check that propor vote outcome is provided?
-        assert False, "UpdateParams not implemented yet."
+            total_rewards <= expected_rewards
+        ), "Paid out amount is more than expected."
 
     else:
         assert False, "Invalid redeemer."
-
-    desired_next_state = StakingState(
-        previous_state.params,
-        previous_state.emission_rate,
-        redeemer.current_time,
-        desired_amount_staked,
-        compute_updated_cumulative_reward_per_token(
-            previous_state.cumulative_reward_per_token,
-            previous_state.emission_rate,
-            previous_state.last_update_time,
-            redeemer.current_time,
-        ),
-    )
-    assert (
-        desired_next_state == next_stake_state
-    ), "Staking state not updated correctly."
