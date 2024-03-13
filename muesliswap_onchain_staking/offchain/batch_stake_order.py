d36b1182c73cec82a27dcca36910bba8d66c855a
PlutusDevServer <contact@muesliswap.com>
2024-03-13 14:35:47 +0000

    add batcher

--- muesliswap_onchain_staking/offchain/batch_stake_order.py
+++ muesliswap_onchain_staking/offchain/batch_stake_order.py
@@ -1,6 +1,6 @@
 import fire
 
-from muesliswap_onchain_staking.onchain import batching
+from muesliswap_onchain_staking.onchain import batching, stake_state, staking
 from muesliswap_onchain_staking.utils.network import show_tx, context
 from muesliswap_onchain_staking.utils import get_signing_info, network, to_address
 from muesliswap_onchain_staking.utils.contracts import get_contract, module_name
@@ -21,24 +21,34 @@ from util import (
 
 
 def main(
-    wallet: str = "staker",
-    stake_token: Token = token_from_string(
-        "672ae1e79585ad1543ef6b4b6c8989a17adcea3040f77ede128d9217.6d7565736c69"
-    ),
-    stake_amount: int = 42,
-    pool_id: Token = token_from_string("abcd.abcd"),
+    wallet: str = "batcher",
 ):
-    _, _, stake_order_batching = get_contract(
+    _, _, batching_address = get_contract(
         module_name(batching), False
     )  # TODO: change to compressed
+    _, _, stake_state_address = get_contract(
+        module_name(stake_state), False
+    )  # TODO: change to compressed
+    _, _, staking_address = get_contract(
+        module_name(staking), False
+    )  # TODO: change to compressed
+
     _, payment_skey, payment_address = get_signing_info(wallet, network=network)
     payment_utxos = context.utxos(payment_address)
 
-    # construct the stake order datum
-    stake_order_datum = batching.AddStakeOrder(
-        owner=to_address(payment_address),
-        pool_id=pool_id,
+    batching_utxos = context.utxos(batching_address)
+    stake_state_utxos = context.utxos(stake_state_address)
+
+    add_stake_order = batching.AddStakeOrder.from_cbor(
+        batching_utxos[0].output.datum.cbor
     )
+    assert len(stake_state_utxos) == 1, "There should be exactly one stake state UTxO."
+    prev_stake_state = stake_state.StakingState.from_cbor(
+        stake_state_utxos[0].output.datum.cbor
+    )
+
+    # TODO construct ApplyOrder and ApplyOrders redeemer
+    # TODO construct datums for new stake_state and staking_position
 
     # build the transaction
     builder = TransactionBuilder(context)
@@ -51,15 +61,15 @@ def main(
         builder.add_input(u)
 
     # construct the output
-    stake_order_output = TransactionOutput(
-        address=stake_order_batching,
-        amount=Value(
-            multi_asset=asset_from_token(stake_token, stake_amount),
-        ),
-        datum=stake_order_datum,
+    stake_state_output = TransactionOutput(
+        address=stake_state_address,
+        #        amount=Value(
+        #            multi_asset=asset_from_token(stake_token, stake_amount),
+        #        ),
+        #        datum=stake_order_datum,
     )
 
-    builder.add_output(with_min_lovelace(stake_order_output, context))
+    builder.add_output(with_min_lovelace(stake_state_output, context))
     builder.ttl = context.last_block_slot + 100
 
     # sign the transaction
