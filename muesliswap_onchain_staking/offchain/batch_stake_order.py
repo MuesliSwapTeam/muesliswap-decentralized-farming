f8ac23408fda5f90d63d84a3adac22fb5b91d27a
PlutusDevServer <contact@muesliswap.com>
2024-03-24 16:58:08 +0000

    batch multiple orders

--- muesliswap_onchain_staking/offchain/batch_stake_order.py
+++ muesliswap_onchain_staking/offchain/batch_stake_order.py
@@ -41,25 +41,29 @@ def main(
     print("Stake state UTxOs:", stake_state_utxos)
     print("Batching UTxOs:", batching_utxos)
 
-    assert len(batching_utxos) == 1, "There should be exactly one batching UTxO."
-    batching_input = batching_utxos[0]
     assert len(stake_state_utxos) == 1, "There should be exactly one stake state UTxO."
     stake_state_input = stake_state_utxos[0]
-
-    add_stake_order_datum = batching.AddStakeOrder.from_cbor(
-        batching_input.output.datum.cbor
-    )
     prev_stake_state_datum = stake_state.StakingState.from_cbor(
         stake_state_input.output.datum.cbor
     )
 
-    tx_inputs = sorted_utxos([batching_input, stake_state_input] + payment_utxos)
-    order_input_index = tx_inputs.index(batching_input)
+    tx_inputs = sorted_utxos(batching_utxos + [stake_state_input] + payment_utxos)
+    order_inputs = sorted(
+        [
+            (tx_inputs.index(u), batching.AddStakeOrder.from_cbor(u.output.datum.cbor), u.output.amount)
+            for u in batching_utxos
+        ]
+    )
+
     stake_state_input_index = tx_inputs.index(stake_state_input)
     current_time = int(datetime.now().timestamp() * 1000)
-
-    amount_to_stake = amount_of_token_in_value(
-        prev_stake_state_datum.params.stake_token, batching_input.output.amount
+    total_amount_of_new_stake = sum(
+        [
+            amount_of_token_in_value(
+                prev_stake_state_datum.params.stake_token, u
+            )
+            for u in batching_utxos
+        ]
     )
 
     # construct redeemers
@@ -70,9 +74,8 @@ def main(
         stake_state.ApplyOrders(
             state_input_index=stake_state_input_index,
             state_output_index=0,
-            order_input_index=order_input_index,
-            order_output_index=1,
-            # license_input_index=0,
+            order_input_indices=[o[0] for o in order_inputs],
+            order_output_indices=[i + 1 for i in range(len(order_inputs))],
             current_time=current_time,
         )
     )
@@ -89,15 +92,18 @@ def main(
         params=prev_stake_state_datum.params,
         emission_rates=prev_stake_state_datum.emission_rates,
         last_update_time=current_time,
-        amount_staked=prev_stake_state_datum.amount_staked + amount_to_stake,
+        amount_staked=prev_stake_state_datum.amount_staked + total_amount_of_new_stake,
         cumulative_rewards_per_token=new_cumulative_pool_rpts,
     )
-    staking_position_datum = staking.StakingPosition(
-        owner=add_stake_order_datum.owner,
-        pool_id=add_stake_order_datum.pool_id,
-        staked_since=current_time,
-        cumulative_pool_rpts_at_start=new_cumulative_pool_rpts,
-    )
+    staking_position_datums = [
+        staking.StakingPosition(
+            owner=d.owner,
+            pool_id=d.pool_id,
+            staked_since=current_time,
+            cumulative_pool_rpts_at_start=new_cumulative_pool_rpts,
+        )
+        for d in [o[1] for o in order_inputs]
+    ]
 
     # construct outputs
     stake_state_output = TransactionOutput(
@@ -105,12 +111,15 @@ def main(
         amount=stake_state_input.output.amount,
         datum=stake_state_datum,
     )
-    staking_position_output = TransactionOutput(
-        address=staking_address,
-        amount=batching_input.output.amount,
-        datum=staking_position_datum,
-    )
-
+    staking_position_outputs = [
+        TransactionOutput(
+            address=stake_state_address,
+            amount=order_inputs[i][2].output.amount,
+            datum=d,
+        )
+        for i, d in enumerate(staking_position_datums)
+    ]
+    
     # build the transaction
     builder = TransactionBuilder(context)
     builder.auxiliary_data = AuxiliaryData(
@@ -120,7 +129,8 @@ def main(
     )
     # - add outputs
     builder.add_output(with_min_lovelace(stake_state_output, context))
-    builder.add_output(with_min_lovelace(staking_position_output, context))
+    for o in staking_position_outputs:
+        builder.add_output(with_min_lovelace(o, context))
     builder.validity_start = context.last_block_slot - 50
     builder.ttl = context.last_block_slot + 100
     # - add inputs
@@ -133,12 +143,13 @@ def main(
         None,
         stake_state_apply_redeemer,
     )
-    builder.add_script_input(
-        batching_input,
-        batching_script,
-        None,
-        batching_apply_redeemer,
-    )
+    for o in order_inputs:
+        builder.add_script_input(
+            o[2],
+            batching_script,
+            None,
+            batching_apply_redeemer,
+        )
 
     # sign the transaction
     signed_tx = builder.build_and_sign(
